6c89760a46883aff4873feb18dd8d2a0aa2c0bef
thl <franktian424@qq.com>
2025-04-02 18:42:14 +0800

    add body_ratio gui

--- scripts/generate_retarget_params.py
+++ scripts/generate_retarget_params.py
@@ -23,17 +23,24 @@ retarget_params = {
         "right_foot": None,
         "foot_height": 0,
     },
-    "whole_body_ratio": 1.0,
-    "body_ratio_dict": {}
+    "whole_body_ratio": [1., 1., 1.],
+    "body_ratio_dict": {},
+    "body_rotate_dict": {}
 }
 model: Optional[mujoco.MjModel] = None
 data: Optional[mujoco.MjData] = None
 viewer: Optional[mujoco.viewer.Handle] = None
 human_generator: Optional[RetargetingMJCFGenerator] = None
 robot_generator: Optional[UnifiedMJCFGenerator] = None
+generator: Optional[MJCFGeneratorComposite] = None
+
+lock = threading.Lock()
+
+body_ratio_groups = []
 
 def check_and_update_base_pose(target):
     global retarget_params, data, model, human_generator, robot_generator
+    # import pdb; pdb.set_trace()
 
     if target == "human":
         base_name = human_generator.all_body_names[0]
@@ -55,11 +62,11 @@ def simulation_loop():
     global retarget_params, data, model, viewer
     assert None not in [data, model, viewer]
     while viewer.is_running():
-        check_and_update_base_pose("robot")
-        check_and_update_base_pose("human")
-
-        mujoco.mj_forward(model, data)
-        viewer.sync()
+        with lock:
+            check_and_update_base_pose("robot")
+            check_and_update_base_pose("human")
+            mujoco.mj_forward(model, data)
+            viewer.sync()
 
 def update_height_callback(sender, app_data, user_data):
     global retarget_params
@@ -69,6 +76,20 @@ def update_foot_name_callback(sender, app_data, user_data):
     global retarget_params
     robot_or_human, left_or_right = user_data.split("_")
     retarget_params[robot_or_human][f"{left_or_right}_foot"] = app_data
+
+def update_body_ratio_callback(sender, app_data, user_data):
+    global retarget_params
+    retarget_params["whole_body_ratio"][user_data["idx"]] = app_data
+
+def refresh_human_model_callback(sender, app_data, user_data):
+    global retarget_params, data, model, human_generator, robot_generator, generator, viewer
+    human_generator.whole_body_ratio = retarget_params["whole_body_ratio"]
+    generator.build()
+    with lock:
+        model = mujoco.MjModel.from_xml_string(generator.mjcf_str)
+        data = mujoco.MjData(model)
+        viewer.close()
+        viewer = mujoco.viewer.launch_passive(model, data)
 
 def show_body_tree_callback(sender, app_data, user_data):
     global human_generator, robot_generator
@@ -82,27 +103,67 @@ def show_body_tree_callback(sender, app_data, user_data):
     with dpg.window(height=300, width=300, horizontal_scrollbar=True, label=f"{user_data} body tree"):
         dpg.add_text(text)
 
+def create_three_slider(prefix_label="", labels=("x", "y", "z"), user_data=None, **kwargs):
+    for i, label in enumerate(labels):
+        dpg.add_slider_float(label=f"{prefix_label}{label}", **kwargs, user_data={
+            "idx": i, "name": prefix_label, "user_data": user_data
+        })
+
+def add_body_ratio_callback(sender, app_data, all_body_names):
+    group_id = f"body_ratio_{len(body_ratio_groups)}"
+
+    def update_name(sender, app_data, user_data):
+        retarget_params["body_ratio_dict"][user_data]["name"] = app_data
+
+    def update_value(sender, app_data, user_data):
+        g_id, idx = user_data["user_data"], user_data["idx"]
+        retarget_params["body_ratio_dict"][g_id]["values"][idx] = app_data
+
+    def remove_component(sender, app_data, user_data):
+        dpg.delete_item(user_data)
+        del retarget_params["body_ratio_dict"][user_data]
+        body_ratio_groups.remove(user_data)
+
+    with dpg.group(parent="body_ratio_group", tag=group_id):
+        with dpg.group(horizontal=True):
+            dpg.add_combo(items=all_body_names, callback=update_name, user_data=group_id)
+            dpg.add_button(label="Remove",callback=remove_component, user_data=group_id)
+        create_three_slider(callback=update_value, user_data=group_id, min_value=0.5, max_value=1.5, default_value=1.)
+        retarget_params["body_ratio_dict"][group_id] = {"name": None, "values": [1.0, 1.0, 1.0]}
+        body_ratio_groups.append(group_id)
+
+
 def create_gui():
     global retarget_params, data, model, viewer, human_generator, robot_generator
     robot_all_body_names = robot_generator.all_body_names
     human_all_body_names = human_generator.all_body_names
 
-
     dpg.create_context()
     with dpg.window(label="main", width=500, height=400):
-
-
-        dpg.add_text("Robot info")
-        dpg.add_button(label="show robot body tree", callback=show_body_tree_callback, user_data="robot")
-        dpg.add_combo(label="left foot name", items=robot_all_body_names, callback=update_foot_name_callback, user_data="robot_left")
-        dpg.add_combo(label="right foot name", items=robot_all_body_names, callback=update_foot_name_callback, user_data="robot_right")
-        dpg.add_slider_float(label="foot height", callback=update_height_callback, user_data="robot", max_value=0.2)
-
-        dpg.add_text("Human info")
-        dpg.add_button(label="show robot body tree", callback=show_body_tree_callback, user_data="human")
-        dpg.add_combo(label="left foot name", items=human_all_body_names, callback=update_foot_name_callback, user_data="human_left")
-        dpg.add_combo(label="right foot name", items=human_all_body_names, callback=update_foot_name_callback, user_data="human_right")
-        dpg.add_slider_float(label="foot height", callback=update_height_callback, user_data="human", max_value=0.2)
+        with dpg.group():
+            with dpg.group(horizontal=True):
+                dpg.add_text("Retargeting Parameters")
+                dpg.add_button(label="show robot body tree", callback=show_body_tree_callback, user_data="robot")
+            dpg.add_combo(label="left foot name", items=robot_all_body_names, callback=update_foot_name_callback, user_data="robot_left")
+            dpg.add_combo(label="right foot name", items=robot_all_body_names, callback=update_foot_name_callback, user_data="robot_right")
+            dpg.add_slider_float(label="foot height", callback=update_height_callback, user_data="robot", min_value=0, max_value=0.2, default_value=0.)
+        dpg.add_separator()
+        with dpg.group():
+            with dpg.group(horizontal=True):
+                dpg.add_text("Human info")
+                dpg.add_button(label="show robot body tree", callback=show_body_tree_callback, user_data="human")
+            dpg.add_combo(label="left foot name", items=human_all_body_names, callback=update_foot_name_callback, user_data="human_left")
+            dpg.add_combo(label="right foot name", items=human_all_body_names, callback=update_foot_name_callback, user_data="human_right")
+            dpg.add_slider_float(label="foot height", callback=update_height_callback, user_data="human", min_value=0, max_value=0.2, default_value=0.)
+        dpg.add_separator()
+        with dpg.group(tag="body_ratio_group"):
+            with dpg.group(horizontal=True):
+                dpg.add_text("human body ratio")
+                dpg.add_button(label="refresh mujoco", callback=refresh_human_model_callback)
+                dpg.add_button(label="Add Component", callback=add_body_ratio_callback, user_data=human_all_body_names)
+            dpg.add_text("whole body")
+            create_three_slider(callback=update_body_ratio_callback, min_value=0.5, max_value=1.5, default_value=1.)
+        dpg.add_separator()
 
     dpg.create_viewport(title='MuJoCo Control', width=800, height=600)
     dpg.setup_dearpygui()
@@ -116,7 +177,7 @@ def create_gui():
 @click.option("--motion_type", prompt="Enter the motion type")
 @click.option("--motion_path", prompt="Enter the motion path")
 def main(robot_name, motion_type, motion_path):
-    global retarget_params, data, model, viewer, human_generator, robot_generator
+    global retarget_params, data, model, viewer, human_generator, robot_generator, generator
 
     if motion_type.lower() == "bvh":
         human_generator_class = BVH2MJCFGenerator
